4d2ff0d92b140cf3a4808d442d5f5a26adfbbd47
Jan W. <janjaydenw730@gmail.com>
2026-02-08 21:26:50 -0500

    Revert "Moving boot partition to /boot/efi"
    This reverts commit 82699763cd5e314610db67ce4cf05fc8f40026a3.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -18,7 +18,7 @@
       fsType = "ext4";
     };
 
-  fileSystems."/boot/efi" =
+  fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/85D9-4574";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
