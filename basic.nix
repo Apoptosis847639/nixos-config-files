e2b6c1c21a5306bcf9246e4517665f90eea9766f
Jan W. <janjaydenw730@gmail.com>
2026-02-09 21:44:56 -0500

    Basic desktop config, for testing.

---
+++ basic.nix
@@ -0,0 +1,13 @@
+# Basic Desktop Configuration
+{ config, lib, pkgs, ... }:
+
+{
+    imports = [
+        "./base-desktop.nix"
+        "./base-uefi-nowindows.nix"
+        "./modules/kdeplasma.nix"
+        "./modules/sddm.nix"
+        "./modules/devthings.nix"
+        "./modules/unfree.nix"
+    ];
+}
